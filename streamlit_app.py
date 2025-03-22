f937a38add788f37cafede10b3348dd98764f9f7
Ridwan <pgmr.red@gmail.com>
2025-03-23 02:36:55 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -139,7 +139,9 @@ if uploaded_file:
             "Table and Figure Captions": table_issues
         }
         pdf = generate_pdf_report(ref_report, corrected_refs, "", formatting_results)
-        st.download_button("📥 Download PDF", pdf, file_name="QAJ_AI_Report.pdf")
+        #st.download_button("📥 Download PDF", pdf, file_name="QAJ_AI_Report.pdf")
+        st.download_button("📥 Download PDF", data=pdf.getvalue(), file_name="QAJ_AI_Report.pdf", mime="application/pdf")
+
 
 else:
     st.info("Upload a PDF or DOCX research article to begin analysis.")
